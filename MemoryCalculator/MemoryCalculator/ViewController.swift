f74ec45c6ea52ba269e045617e6a46f68e0dacc0
Mykal Cuin <mykcuin@gmail.com>
2015-07-22 15:27:43 -0600

    Added Outlets for all buttons and label as well as connected them together.

--- MemoryCalculator/MemoryCalculator/ViewController.swift
+++ MemoryCalculator/MemoryCalculator/ViewController.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var numberLabel : UILabel!
+    @IBOutlet var upArrow : UIButton!
+    @IBOutlet var downArrow : UIButton!
+    @IBOutlet var clearButton : UIButton!
+    @IBOutlet var divideButton : UIButton!
+    @IBOutlet var openParenthesisButton : UIButton!
+    @IBOutlet var closeParenthesisButton : UIButton!
+    @IBOutlet var decimalButton : UIButton!
+    @IBOutlet var multiplyButton : UIButton!
+    @IBOutlet var sevenButton : UIButton!
+    @IBOutlet var eightButton : UIButton!
+    @IBOutlet var nineButton : UIButton!
+    @IBOutlet var subtractButton : UIButton!
+    @IBOutlet var fourButton : UIButton!
+    @IBOutlet var fiveButton : UIButton!
+    @IBOutlet var sixButton : UIButton!
+    @IBOutlet var addButton : UIButton!
+    @IBOutlet var oneButton : UIButton!
+    @IBOutlet var twoButton : UIButton!
+    @IBOutlet var threeButton : UIButton!
+    @IBOutlet var equalsButton : UIButton!
+    @IBOutlet var zeroButton : UIButton!
+    @IBOutlet var deleteButton : UIButton!
+    @IBOutlet var squareRootButton : UIButton!
+    @IBOutlet var squareButton : UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
